e3a7af7d184ed6790d76ca220f84c9a9dc7872e1
damrem <damien.remars@gmail.com>
2014-09-11 11:42:22 +0200

    exploring foursquare venues and showing json items for each one

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the damienRemarsTestWopataApp
  */
 angular.module('damienRemarsTestWopataApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', '$resource', function ($scope, $resource) {
+    $resource('https://api.foursquare.com/v2/venues/explore').get({
+    	near:"Bordeaux", 
+    	oauth_token:"RPLFI1IXWXIPGWFQQ52WNVI00Q5KBOL1EIQMQB1UJM1QYSUZ",
+    	v:"20140911"
+    }, function(callback){
+    	console.log(callback);
+    	$scope.items = callback.response.groups[0].items;
+    });
+  }]);
